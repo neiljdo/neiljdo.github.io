a53898de511d7a3a4af15d3bf461125eff0b0c4c
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2024-01-09 00:28:08 +0000

    Remove prepopulated posts

--- next.config.js
+++ next.config.js
@@ -74,15 +74,17 @@ module.exports = () => {
           hostname: 'picsum.photos',
         },
       ],
+
+      unoptimized: true,
     },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: securityHeaders,
-        },
-      ]
-    },
+    // async headers() {
+    //   return [
+    //     {
+    //       source: '/(.*)',
+    //       headers: securityHeaders,
+    //     },
+    //   ]
+    // },
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
